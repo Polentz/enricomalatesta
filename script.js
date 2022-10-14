59676ea81513d6583a8adf1854c8204356bea032
Giulia Polenta <giuliapolenta@gmail.com>
2022-10-14 15:46:30 +0200

    requested changes

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-window.onload = init;
+const about = document.getElementById("about");
+const aboutOpenBtn = document.getElementById("about-btn");
+const aboutcloseBtn = document.getElementById("about-cls");
+const aboutBtnMobile = document.getElementById("about-btn-mobile")
+const info = document.getElementById("info");
+const infoOpenBtn = document.getElementById("info-btn");
+const infoCloseBtn = document.getElementById("info-cls");
+const infoBtnMobile = document.getElementById("info-btn-mobile");
+const menu = document.getElementById("menu-mobile");
+const menuBtn = document.getElementById("menu-btn");
 
-function init() {
-    initMouseClick();
+
+const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty("--doc-height", `${window.innerHeight}px`)
+}
+window.addEventListener("resize", documentHeight)
+documentHeight();
+
+const functions = () => {
+    toggleClick(aboutOpenBtn, about);
+    toggleClick(aboutcloseBtn, about);
+    toggleClick(aboutBtnMobile, about);
+    if (info) {
+        toggleClick(infoOpenBtn, info);
+        toggleClick(infoCloseBtn, info);
+        toggleClick(infoBtnMobile, info);
+    }
+    toggleClick(menuBtn, menu);
 }
 
-function initMouseClick() {
-    toggleClassOnClick("about-btn", "about", "open");
-    toggleClassOnClick("about-cls", "about", "open");
-    toggleClassOnClick("about-btn-mobile", "about", "open");
-
-    openMobileMenu("menu-btn", "menu-mobile", "open");
-}
-
-function toggleClassOnClick(trigger, element, className) {
-    document.getElementById(trigger).addEventListener("click", (event) => {
-        document.getElementById(element).classList.toggle(className);
-        // document.getElementById(otherElement).classList.toggle(otherClassName);
+const toggleClick = (button, element) => {
+    button.addEventListener("click", (event) => {
+        element.classList.toggle("open");
         event.stopPropagation();
     });
 }
 
-function openMobileMenu(button, menu, className) {
-    document.getElementById(button).addEventListener("click", (event) => {
-        document.getElementById(menu).classList.toggle(className);
-        event.stopPropagation();
-    });
-}
+functions();
