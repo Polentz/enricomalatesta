3f76732734402db7e14155a1a41de16b9d0da66e
Giulia Polenta <giuliapolenta@gmail.com>
2023-03-01 14:52:55 +0100

    Complete caveja

--- script.js
+++ script.js
@@ -11,54 +11,72 @@ const menuBtn = document.getElementById("menu-btn");
 const menuLabels = document.querySelectorAll(".link-mobile");
 const items = document.querySelectorAll(".item");
 
-
 const documentHeight = () => {
     const doc = document.documentElement
     doc.style.setProperty("--doc-height", `${window.innerHeight}px`)
-}
+};
 window.addEventListener("resize", documentHeight)
 documentHeight();
 
 const functions = () => {
     toggleClick(aboutOpenBtn, about);
-    toggleClick(aboutcloseBtn, about);
-    toggleClick(aboutBtnMobile, about);
+    closeClick(aboutcloseBtn, about);
+    toggleClickMobile(aboutBtnMobile, about);
     if (info) {
         toggleClick(infoOpenBtn, info);
-        toggleClick(infoCloseBtn, info);
-        toggleClick(infoBtnMobile, info);
+        closeClick(infoCloseBtn, info);
+        toggleClickMobile(infoBtnMobile, info);
     }
     toggleClick(menuBtn, menu);
-    closeClick(menuLabels, menu);
+    closeAll(menuLabels, menu);
 };
 
 const toggleClick = (button, element) => {
-    button.addEventListener("click", (event) => {
+    button.addEventListener("click", () => {
         element.classList.toggle("open");
-        event.stopPropagation();
+        if (menuBtn.innerHTML == "CLOSE") {
+            menuBtn.innerHTML = "MENU";
+        } else {
+            menuBtn.innerHTML = "CLOSE";
+        };
     });
 };
 
-const closeClick = (buttons, element) => {
+const closeClick = (button, element) => {
+    button.addEventListener("click", () => {
+        element.classList.remove("open");
+        menuBtn.innerHTML = "MENU";
+    });
+};
+
+const toggleClickMobile = (button, element) => {
+    button.addEventListener("click", () => {
+        element.classList.toggle("open");
+    });
+};
+
+const closeAll = (buttons, element) => {
     buttons.forEach(button => {
-        button.addEventListener("click", (event) => {
+        button.addEventListener("click", () => {
             element.classList.remove("open");
-            event.stopPropagation();
-        })
+            menuBtn.innerHTML = "MENU";
+        });
     });
 };
 
 items.forEach(item => {
-    const open = item.querySelector(".item-title");
+    const open = item.querySelectorAll(".item-title, .cms-opener");
     const content = item.querySelector(".item-content");
-    const close = item.querySelector(".info-close");
-    open.addEventListener("click", () => {
-        let randomLeft = Math.floor(Math.random() * 55);
-        let randomTop = Math.floor(Math.random() * 20);
-        content.style.setProperty("--random-left", `${randomLeft}` + "%");
-        content.style.setProperty("--random-top", `${randomTop}` + "%");
-        content.classList.toggle("open");
-    });
+    const close = item.querySelector(".item-content-close");
+    open.forEach(opener => {
+        opener.addEventListener("click", () => {
+            let randomLeft = Math.floor(Math.random() * 55);
+            let randomTop = Math.floor(Math.random() * 20);
+            content.style.setProperty("--random-left", `${randomLeft}` + "%");
+            content.style.setProperty("--random-top", `${randomTop}` + "%");
+            content.classList.toggle("open");
+        });
+    })
     close.addEventListener("click", () => {
         content.classList.remove("open");
     });
